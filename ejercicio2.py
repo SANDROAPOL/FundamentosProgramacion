3e8a8e7c03c6c47b73f093eb7803358e566b5e94
SANDROAPOL <sandroapolinar80@gmail.com>
2024-10-07 11:58:13 -0600

    ejercicio 1 y 2

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+##Programa que calcula el area y perimetro de un rectangulo
+
+base=input("escribe la base del rectangulo")
+base=int(base)
+altura=input("escribe la altura")
+altura=int(altura)
+area=base*altura
+perimetro=(base+altura)*2
+print("El area del rectangulo es",area)
+print("El perimetro del rectangulo es",perimetro)
+
+
+ 
