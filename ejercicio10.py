7d431ed2247d63d6606fc4af168020f8bd4e721c
SANDROAPOL <sandroapolinar80@gmail.com>
2024-10-14 10:48:17 -0600

    ejercicios

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,5 +1,8 @@
 # 10. Calificación final
-promedio_parciales = float(input("Ingrese el promedio de sus tres parciales: "))
+c1= float(input("Ingrese el promedio de primer parcial: "))
+c2= float(input("Ingrese el promedio de segundo parcial: "))
+c3= float(input("Ingrese el promedio de tercer parcial: "))
+promedio_parciales = (c1+c2+c3)/3
 examen_final = float(input("Ingrese la calificación del examen final: "))
 trabajo_final = float(input("Ingrese la calificación del trabajo final: "))
 calificacion_final = (promedio_parciales * 0.55) + (examen_final * 0.30) + (trabajo_final * 0.15)
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,5 @@
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
 print("Suma:", num1 + num2)
 print("Resta:", num1 - num2)
 print("Multiplicación:", num1 * num2)
@@ -7,3 +7,4 @@ if num2 != 0:
     print("División:", num1 / num2)
 else:
     print("División: No es posible dividir entre 0")
+    
